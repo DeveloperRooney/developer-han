0167a4514755606c6553506f18aef455394f8711
DeveloperRooney <han_0427@naver.com>
2021-06-12 01:07:43 +0900

    js 유틸 생성

--- util.php
+++ util.php
@@ -1,9 +1,23 @@
 <?php
 
+session_start();
+
+// js 관련 함수
 function jsAlert($msg, $location = null) {
     echo "<script>";
-    echo "alert('$msg')";
+    echo "alert('$msg');";
+    if($location) {
+        echo "location.href='$location';";
+    }
     echo "</script>";
+}
+
+// 권한 검사
+
+function loginCk() {
+    if(!isset($_SESSION['user'])) {
+        jsAlert("로그인 후 이용해 주세요.");
+    }
 }
 
 ?>
