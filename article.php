d06a773d5205b97d5c12786db5532b8be508d3d7
DeveloperRooney <han_0427@naver.com>
2021-06-12 14:45:37 +0900

    게시판 리스트 페이지

---
+++ article.php
@@ -0,0 +1,83 @@
+<?php require_once "head.php";?>
+
+<div class="wrapper">
+    <?php require_once "header.php";?>
+
+
+
+    <div class="layout">
+        <div class="article-list">
+            <h1>BOARD</h1>
+            <h2>전체글입니다. 우측에서 게시판을 선택해주세요.</h2>
+            <select>
+                <option>전체글</option>
+                <option>자유 게시판</option>
+                <option>취미 게시판</option>
+            </select>
+            <section>
+                <ul>
+                    <li>글번호</li>
+                    <li>제목</li>
+                    <li>작성자</li>
+                    <li>작성일자</li>
+                    <li>조회수</li>
+                </ul>
+                <ul>
+                    <li>1</li>
+                    <li>자바는 어떻게 배울까?</li>
+                    <li>김수한</li>
+                    <li>2021.06.12</li>
+                    <li>100</li>
+                </ul>
+                <ul>
+                    <li>1</li>
+                    <li>자바는 어떻게 배울까?</li>
+                    <li>김수한</li>
+                    <li>2021.06.12</li>
+                    <li>100</li>
+                </ul>
+                <ul>
+                    <li>1</li>
+                    <li>자바는 어떻게 배울까?</li>
+                    <li>김수한</li>
+                    <li>2021.06.12</li>
+                    <li>100</li>
+                </ul>
+                <ul>
+                    <li>1</li>
+                    <li>자바는 어떻게 배울까?</li>
+                    <li>김수한</li>
+                    <li>2021.06.12</li>
+                    <li>100</li>
+                </ul>
+                <ul>
+                    <li>1</li>
+                    <li>자바는 어떻게 배울까?</li>
+                    <li>김수한</li>
+                    <li>2021.06.12</li>
+                    <li>100</li>
+                </ul>
+                <ul>
+                    <li>1</li>
+                    <li>자바는 어떻게 배울까?</li>
+                    <li>김수한</li>
+                    <li>2021.06.12</li>
+                    <li>100</li>
+                </ul>
+                <ul>
+                    <li>1</li>
+                    <li>자바는 어떻게 배울까?</li>
+                    <li>김수한</li>
+                    <li>2021.06.12</li>
+                    <li>100</li>
+                </ul>
+            </section>
+        </div>
+    </div>
+
+
+
+
+
+    <?php require_once "footer.php";?>
+</div>
--- util.php
+++ util.php
@@ -1,22 +1,40 @@
 <?php
 
+// 세션 사용
 session_start();
 
+
 // js 관련 함수
-function jsAlert($msg, $location = null) {
+function jsAlert($msg) {
     echo "<script>";
     echo "alert('$msg');";
-    if($location) {
-        echo "location.href='$location';";
+    echo "</script>";
+}
+
+function jsHistoryBack($msg = null) {
+    if ($msg) {
+        jsAlert($msg);
     }
+    echo "<script>";
+    echo "history.back();";
+    echo "</script>";
+}
+
+function jsLocationReplace($url, $msg = null) {
+    if ($msg) {
+        jsAlert($msg);
+    }
+    echo "<script>";
+    echo "location.replace('$url')";
     echo "</script>";
 }
 
 // 권한 검사
 
+// 로그인 여부 확인
 function loginCk() {
     if(!isset($_SESSION['user'])) {
-        jsAlert("로그인 후 이용해 주세요.");
+        jsHistoryBack("로그인 후 이용해주세요.");
     }
 }
 
